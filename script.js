c06a892c2b93bf1655e6aac65fd4e464670de232
Sarah-Elizabeth Laws <100695089+akaLaws@users.noreply.github.com>
2022-10-11 11:20:02 +0200

    Updated with comments

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+
+// Lytter om alt content er loaded på document (index.html i vores tilfælde)
+// Når content er loaded skal der udføres en funktion
 document.addEventListener("DOMContentLoaded", function () {
+  
+  // Funktionen lytter på <body> i document om der bliver klikket på den - når der bliver klikket skal den unfører en funktion
   document.querySelector("body").addEventListener("click", function () {
+    
+    // Funktionen tilføjer class'en "animate__tada" til <body> tagget i HTML
     document.querySelector("body").classList.add("animate__tada");
+    
+    // Funktionen sætter også en time out på 500 ms 
     setTimeout(() => {
+      
+      // Når de 500 ms er gået skal class'en animate__tada fjernes fra <body> tagget igen 
       document.querySelector("body").classList.remove("animate__tada");
+    
     }, 500);
   });
 });
